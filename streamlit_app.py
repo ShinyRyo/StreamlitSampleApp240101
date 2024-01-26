64f4126462c1126c9c5d2a21490aa7c07618c115
root <root@MSI>
2024-01-26 11:49:24 +0900

    gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+st.write("Hello, Streamlit!")
 
+number = st.slider('Select a number', 0, 100, 50)
+st.write('Selected number is:', number)
+
+import pandas as pd
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+st.write(df)
